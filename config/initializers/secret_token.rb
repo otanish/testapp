8411498967c8f4c0a11849ce9067b0944073618a
Open Tanish <tanish.sin@gmail.com>
2011-01-15 00:57:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'de846ad9d2c122a1eb972c042bfa327dc723ca3af3d7690b077f6169aeb47f717db603b0e335adb2d9dffcf2e0fb2bdd5672ff1e05efa439299263f74fd897ac'
